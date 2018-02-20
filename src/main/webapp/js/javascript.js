37d358c9d54807b3acab892f3dd8d5d8f7aafbf9
stent <stent@DESKTOP-L35J44P.home>
2018-02-20 15:46:22 +0100

    Website geimplementeerd van Joel

---
+++ src/main/webapp/js/javascript.js
@@ -0,0 +1,12 @@
+/* Zet de klasse anders als er op het icoontje wordt geklikt. */
+function showMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+
